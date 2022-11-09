e95cf4a8ad7bf45963ad8166634265e9b1f06fb5
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-11-09 11:49:45 -0300

    Add: .editorconfig

--- autogit.sh
+++ autogit.sh
@@ -1,7 +1,4 @@
-git init
 git add *
-git commit -m "Inserted project"
-git branch -M main
-git remote add origin https://github.com/CarlosViniMSouza/fullstack-journey-ocean.git
+git commit -m "Add: .editorconfig"
 git push -u origin main
 git status
